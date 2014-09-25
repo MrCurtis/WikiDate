dbf29e774a564e23b2ca6ee326327aa8d4e03067
a <4184070+MrCurtis@users.noreply.github.com>
2014-09-25 17:48:09 +0100

    first commit, the application can already get date information for about 4/5ths of dates

---
+++ wiki_date.js
@@ -0,0 +1,188 @@
+var wikiDate = ( function () {
+
+    var array_of_months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+
+    var year_and_month_page = {
+        url: function (year, month, day){
+            return 'http://en.wikipedia.org/w/api.php?action=parse&format=json&page='+month+'_'+year;
+        },
+
+        parseData: function  (data, year, month, day){
+            var parsedData = null;
+            try{
+                parsedData = data.parse.text['*'];
+            }
+            catch (error) {
+                if (error.name === 'TypeError'){
+                    console.log('TypeError: It looks like your request to wikipedia is not returning the required text.')
+                }else{
+                    console.log('Error in wikipedia date request.');
+                }
+            }
+            var $data_html = $('<div></div>').html(parsedData);
+            var date_string = "[id*='"+month+"_"+day+"\\.2C_"+year+"']";
+            var filtered = $data_html.find(date_string).first();
+            if (filtered.length){
+                var title = filtered.parent();
+                var content =title.next();
+                parsedData = $('<div></div>');
+                parsedData.append(title).append(content);
+                return parsedData;
+            }
+            date_string = "#"+day+"_"+month+"_"+year;
+            filtered = $data_html.find(date_string).first();
+            if (filtered.length){
+                var title = filtered.next('h2');
+                console.log('h2: ', title);
+                var content = title.next('ul');
+                console.log('ul: ', content);
+                if (title.length || content.length){
+                    parsedData = $('<div></div>');
+                    parsedData.append(title).append(content);
+                    return parsedData;
+                }
+            }
+            date_string = "#"+year+"_"+month+"_"+day;
+            filtered = $data_html.find(date_string).first();
+            if (filtered.length){
+                var container = filtered.next('.vevent');
+                if (container.length){
+                    content = container.children('tbody').first();
+                    if (content.length){
+                        parsedData = $('<div></div>');
+                        parsedData.append(title).append(content);
+                        return parsedData;
+                    }
+                }
+            }
+            parsedData = null;
+            return parsedData;
+        },
+    };
+
+    var year_page = {
+        url: function (year, month, day){
+            return 'http://en.wikipedia.org/w/api.php?action=parse&format=json&page='+year;
+        },
+        parseData: function (data, year, month, day){
+            var parsedData = null;
+            try{
+                parsedData = data.parse.text['*'];
+            }
+            catch (error) {
+                if (error.name === 'TypeError'){
+                    console.log('TypeError: It looks like your request to wikipedia is not returning the required text.')
+                }else{
+                    console.log('Error in wikipedia date request.');
+                }
+            }
+            var $data_html = $('<div></div>').html(parsedData);
+            var date_string = "[ title|=\""+month+" "+day+"\"]";
+            filtered = $data_html.find("h2");
+            if (filtered.length){
+                var pData = $('<div></div>');
+                var haveMatch = false;
+                var eventType = ['Events', 'Births', 'Deaths'];
+                filtered.each( function () {
+                    for (var event_index = 0; event_index < eventType.length; event_index++){
+                        if ($(this).children().filter("#"+eventType[event_index]).length){
+                            var events = $(this).nextUntil("h2");
+                            events = events.find(date_string);
+                            if (events.length) {
+                                var title = $('<p></p>').html(eventType[event_index]);
+                                var content = events.first().parent();
+                                pData.append(title).append(content);
+                                haveMatch = true;
+                            }
+                        }
+                    }
+                });
+                if (haveMatch){
+                    return pData;
+                }
+            }
+            return null;
+        },
+    };
+
+    //list of object representing pages to request and their associated parsing function. 
+    //Note that that requests are made in reverse order i.e. array_of_pages[array_of_pages.length-1]
+    //is used first.
+    var array_of_pages = [
+        year_page,   
+        year_and_month_page,
+    ];
+
+    var checkArguments = function (args){
+
+            var arg_len = args.length;
+            if ( !(arg_len === 3 || arg_len === 4) ){
+                    throw new Error('IncorrectNumberOfArgumentsException', 'getWikiInfo has three required arguments and one optional argument, but ' + arg_len + ' arguments given.');
+            }
+            if ( !(typeof args[0] === 'function')){
+                throw new Error('WrongArgumentTypeError', 'First argument (callback) must be a function');
+            }
+            if ( !(typeof +args[1] === 'number' && isFinite(+args[1]) && +args[1]%1 === 0) ){
+                throw new Error('WrongArgumentTypeError', 'Second argument (year) must be an integer');
+            }
+            if ( $.inArray(args[2], array_of_months) === -1 ){
+                throw new Error('WrongArgumentTypeError', 'Third argument (month) must be a full month name, begining with capital letter, e.g. \'January\'.');
+            }
+            if ( arg_len === 4 && !(typeof +args[3] === 'number' && isFinite(+args[3]) && +args[3]%1 === 0)){
+                throw new Error('WrongArgumentTypeError', 'Fourth argument (day) must be an integer');
+            }
+
+    };
+
+    var makeAjax = function (url, parseFunction, callback, year, month, day, array_of_pages_copy){
+        $.ajax({
+            dataType: 'jsonp',
+            url: url,
+            success: function (data) {
+                var wikiInfo = parseFunction(data, year, month, day);
+                if (wikiInfo){
+                    callback(wikiInfo, year, month, day);
+                }else if (array_of_pages_copy.length) {
+                    var page = array_of_pages_copy.pop();
+                    makeAjax(page.url(year, month, day), page.parseData, callback, year, month, day, array_of_pages_copy);
+                }else{
+                    callback('No info for this date.', year, month, day);
+                }
+            },
+            error: function () {
+                console.log('Not working');
+            },
+        });
+    };
+
+    var getWikiInfo = function (callback, year, month, day) {
+        checkArguments(arguments);
+        var array_of_pages_copy = array_of_pages.slice();
+        var page = array_of_pages_copy.pop();
+        makeAjax(page.url(year, month, day), page.parseData, callback, year, month, day, array_of_pages_copy);
+    };
+
+    Error: function Error (name, message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    return {
+        getWikiInfo: getWikiInfo,
+        Error: Error,
+    }
+
+})();
---
+++ wiki_date_test.js
@@ -0,0 +1,87 @@
+var wiki_date_random_date = ( function (seed){
+    var seed = seed;
+    var array_of_months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+    var rng = function (n) {
+        //returns an integer in the range 0,1,...,n-2,n-1
+        var x = (1 + Math.sin(seed))*10000;
+        seed = seed + 1;
+        x = x - (x%1);
+        x = x % n;
+        return x;
+    };
+    var rnd_month = function () {
+        var x = rng(12);
+        return array_of_months[x];
+    };
+    var rnd_date = function () {
+        var year = rng(115) + 1900;
+        var month = rnd_month();
+        var day = rng(28) + 1;
+        return{
+            year: year,
+            month: month,
+            day: day,
+        }
+    }
+    return rnd_date;
+})(4817);
+
+QUnit.test( "wiki_date inputs", function( assert ){ 
+    console.log('inputs being tested');
+    assert.throws(function() {wikiDate.getWikiInfo(1,2,3,4,5);}, new wikiDate.Error('IncorrectNumberOfArgumentsException', 'getWikiInfo has three required arguments and one optional argument, but 5 arguments given.'));
+    assert.throws(function() {wikiDate.getWikiInfo(1,2);}, new wikiDate.Error('IncorrectNumberOfArgumentsException', 'getWikiInfo has three required arguments and one optional argument, but 2 arguments given.'));
+    assert.throws(function() {wikiDate.getWikiInfo(1,2,3,4);}, new wikiDate.Error('WrongArgumentTypeError', 'First argument (callback) must be a function'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,NaN,3,4);}, new wikiDate.Error('WrongArgumentTypeError', 'Second argument (year) must be an integer'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,0.2,3,4);}, new wikiDate.Error('WrongArgumentTypeError', 'Second argument (year) must be an integer'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,'First',3,4);}, new wikiDate.Error('WrongArgumentTypeError', 'Second argument (year) must be an integer'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,'2001a',3,4);}, new wikiDate.Error('WrongArgumentTypeError', 'Second argument (year) must be an integer'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,2014,3,4);}, new wikiDate.Error('WrongArgumentTypeError', 'Third argument (month) must be a full month name, begining with capital letter, e.g. \'January\'.'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,2014,'Jan',4);}, new wikiDate.Error('WrongArgumentTypeError', 'Third argument (month) must be a full month name, begining with capital letter, e.g. \'January\'.'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,2014,'january',4);}, new wikiDate.Error('WrongArgumentTypeError', 'Third argument (month) must be a full month name, begining with capital letter, e.g. \'January\'.'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,2014,'January','1st');}, new wikiDate.Error('WrongArgumentTypeError', 'Fourth argument (day) must be an integer'));
+    assert.throws(function() {wikiDate.getWikiInfo(wikiDate.getWikiInfo,'2001','January','23a');}, new wikiDate.Error('WrongArgumentTypeError', 'Fourth argument (day) must be an integer'));
+});
+
+QUnit.asyncTest( "wiki_date coverage", function( assert ){ 
+    QUnit.expect(100);
+    QUnit.stop(99);
+    function addContent (content, successful, year, month, day) {
+        var date_details = year+'_'+month+'_'+day;
+        var date_p = $('<p></p>').html(date_details);
+        var date_info = $('<div></div>').html(content);
+        if (successful){
+            date_p.css('color', 'green');
+            $('#successful_content_div').append(date_p);
+            $('#successful_content_div').append(date_info);
+        }else{
+            date_p.css('color', 'red');
+            $('#failed_content_div').append(date_p);
+            $('#failed_content_div').append(date_info);
+        }
+    }
+    function theCallback (response, year, month, day) {
+        var successful = !(response == 'No info for this date.' );
+        addContent(response, successful, year, month, day);
+        assert.notEqual(response, 'No info for this date.');
+        QUnit.start();
+    }
+
+    for (var i = 0; i < 100; i++) {
+        var date = wiki_date_random_date();
+        wikiDate.getWikiInfo(theCallback, date.year, date.month, date.day);
+    }
+
+});
